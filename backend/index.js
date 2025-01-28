9822e9482035629e99c321dc687e3aefb9ff406e
Yuvraj Tiwari <tiwariyuvraj9109@gmail.com>
2025-01-28 16:00:33 +0530

    ready for deployment

--- backend/index.js
+++ backend/index.js
@@ -28,8 +28,10 @@ app.use(cors(corsOption));
  app.use("/api/v1/message",messageRoute);
 
 app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+
 app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+    res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
 });
 
 
